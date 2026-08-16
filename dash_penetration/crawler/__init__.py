27e2225b644f2c212378e6a41c4b9060cbf21524
edgar.treischl <edgar.treischl@isb.bayern.de>
2026-08-16 09:12:05 +0200

    added fun in init

---
+++ dash_penetration/crawler/__init__.py
@@ -0,0 +1,15 @@
+from .urls import (
+    normalize_url, 
+    extract_domain, 
+    is_duplicate, 
+    extract_path_and_query, 
+    URLCache
+)
+
+__all__ = [
+    "normalize_url", 
+    "extract_domain", 
+    "is_duplicate", 
+    "extract_path_and_query",
+    "URLCache"
+    ]
